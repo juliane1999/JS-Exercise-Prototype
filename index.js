37ce0e6bdc3237fa9fc368524ca3f18b12956d2a
juliane1999 <lastjuliane@gmail.com>
2021-06-16 12:23:53 -0700

    passed all 22 tasks for Prototype Project

--- index.js
+++ index.js
@@ -39,12 +39,28 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name,age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
  
+
+Person.prototype.eat = function(edible){
+  if(this.stomach.length <= 10){
+    this.stomach.push(edible);
+  }
+}
  
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
   
   
   
@@ -63,10 +79,19 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model,milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+  }
+  
+
   
   /*
     TASK 3
@@ -75,18 +100,24 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name,age,favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
   }
- 
+
+ Baby.prototype = Object.create(Person.prototype);
+
+ Baby.prototype.play = function(){
+   return `${this.name} is ${this.age} years old and they are playing with their ${this.favoriteToy}`;
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. The window/global object binding has 'this' in the console log so when you invoke the function it returns what you invoked.
+    2. Implicit binding is like having fill in the blanks by placing ${this.property}
+    3. New binding is creating declarations like this.property = value.
+    4. Explicit binding uses .call and .apply.
   */
   
   
